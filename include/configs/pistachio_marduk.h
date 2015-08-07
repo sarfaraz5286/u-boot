3b29aaed225ac1a25e60221489942997ea578289
Ionela Voinescu <ionela.voinescu@imgtec.com>
2015-08-07 16:22:54 +0100

    config: add support for Marduk board
    The pistachio_bub.h config file will keep the default support
    for the pistachio bring up board. This file will be included
    in board specific configuration files, like the pistachio_marduk.h,
    which will replace some of the default CONFIG values with board
    specific values
    
    Change-Id: I8270ad8757a1d4acfe94c1ddd699e910f251f6ce
    Signed-off-by: Ionela Voinescu <ionela.voinescu@imgtec.com>

---
+++ include/configs/pistachio_marduk.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2015 Imagination Technologies
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef _PISTACHIO_CONFIG_MARDUK_H
+#define _PISTACHIO_CONFIG_MARDUK_H
+
+#include <configs/pistachio_bub.h>
+
+#endif /* _PISTACHIO_CONFIG_MARDUK_H */
