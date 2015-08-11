22d67fb62f76fd1d5c8d0c4f3870b80325186e07
Tianci Ma <Tianci.Ma@imgtec.com>
2015-08-11 14:15:44 +0100

    SPL: add build infrastructure for mips32 SPL
    When CONFIG_SPL will be defined in the defconfig file this will
    tell the Makefile to build spl image using the Makefile.spl
    script.
    
    The script looks at u-boot-spl.lds and starts execution from
    start.S file. There the stack and BSS get set up and the function
    "sb" from spl_boot.c get called. That function will further
    provide functionality specific to the SPL (clock and MFIO setup,
    DDR initialization, etc.).
    
    Change-Id: I4377a5ec9d228b90fbe510a5b93454d8ed30647b
    Signed-off-by: Tianci Ma <Tianci.Ma@imgtec.com>

---
+++ arch/mips/include/asm/spl.h
@@ -0,0 +1,25 @@
+/*
+ * (C) Copyright 2015 Imagination Technologies
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#ifndef	_ASM_SPL_H_
+#define	_ASM_SPL_H_
+
+enum {
+	BOOT_DEVICE_RAM,
+	BOOT_DEVICE_MMC1,
+	BOOT_DEVICE_MMC2,
+	BOOT_DEVICE_MMC2_2,
+	BOOT_DEVICE_NAND,
+	BOOT_DEVICE_ONENAND,
+	BOOT_DEVICE_NOR,
+	BOOT_DEVICE_UART,
+	BOOT_DEVICE_SPI,
+	BOOT_DEVICE_SATA,
+	BOOT_DEVICE_I2C,
+	BOOT_DEVICE_BOARD,
+	BOOT_DEVICE_NONE
+};
+
+#endif
---
+++ board/imgtec/pistachio_bub/spl_boot.c
@@ -0,0 +1,16 @@
+/*
+ * Startup Code for MIPS32 CPU-core SPL
+ *
+ * Copyright (c) 2015 Imagination Technologies
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <asm/mipsregs.h>
+#include <common.h>
+
+u32 sb(void)
+{
+	/* Nothing to do for the moment */
+	return 0;
+}
