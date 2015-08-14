73e6dbfcefaacfca6d01e5f9ec254264480697c2
Govindraj Raja <govindraj.raja@imgtec.com>
2015-08-14 11:59:55 +0100

    pistachio: mfio: Add suport for a simple mfio function.
    Add a simple mfio api's that can be used to configure
    peripheral mfio's
    
    Change-Id: Ida296d1dbfecfbc9a94b9efec81fb7d7aa71a878
    Signed-off-by: Govindraj Raja <govindraj.raja@imgtec.com>

---
+++ board/imgtec/pistachio_bub/mfio.h
@@ -0,0 +1,180 @@
+/*
+ * Copyright (C) 2015 Imagination Technologies
+ * Author: Govindraj Raja <govindraj.raja@imgtec.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+/* *** Function Values *** */
+
+#define PRIMARY	    0
+#define FUNCTION_1  1
+#define FUNCTION_2  2
+
+/* *** MFIO Pads and their Function Selects *** */
+#define MFIO_1_FUNC_SEL_START    0
+#define MFIO_1_FUNC_SEL_END      1
+
+#define MFIO_2_FUNC_SEL_START    2
+#define MFIO_2_FUNC_SEL_END      3
+
+#define MFIO_15_FUNC_SEL_START   4
+#define MFIO_15_FUNC_SEL_END     5
+
+#define MFIO_16_FUNC_SEL_START   6
+#define MFIO_16_FUNC_SEL_END     7
+
+#define MFIO_17_FUNC_SEL_START   8
+#define MFIO_17_FUNC_SEL_END     9
+
+#define MFIO_18_FUNC_SEL_START   10
+#define MFIO_18_FUNC_SEL_END     11
+
+#define MFIO_19_FUNC_SEL_START   12
+#define MFIO_19_FUNC_SEL_END     13
+
+#define MFIO_20_FUNC_SEL_START   14
+#define MFIO_20_FUNC_SEL_END     15
+
+#define MFIO_21_FUNC_SEL_START   16
+#define MFIO_21_FUNC_SEL_END     17
+
+#define MFIO_22_FUNC_SEL_START   18
+#define MFIO_22_FUNC_SEL_END     19
+
+#define MFIO_28_FUNC_SEL_START   20
+#define MFIO_28_FUNC_SEL_END     20
+
+#define MFIO_29_FUNC_SEL_START   21
+#define MFIO_29_FUNC_SEL_END     21
+
+#define MFIO_30_FUNC_SEL_START   22
+#define MFIO_30_FUNC_SEL_END     22
+
+#define MFIO_31_FUNC_SEL_START   23
+#define MFIO_31_FUNC_SEL_END     23
+
+#define MFIO_36_FUNC_SEL_START   24
+#define MFIO_36_FUNC_SEL_END     24
+
+#define MFIO_37_FUNC_SEL_START   25
+#define MFIO_37_FUNC_SEL_END     25
+
+#define MFIO_38_FUNC_SEL_START   26
+#define MFIO_38_FUNC_SEL_END     26
+
+#define MFIO_39_FUNC_SEL_START   27
+#define MFIO_39_FUNC_SEL_END     27
+
+#define MFIO_40_FUNC_SEL_START   28
+#define MFIO_40_FUNC_SEL_END     28
+
+#define MFIO_41_FUNC_SEL_START   29
+#define MFIO_41_FUNC_SEL_END     29
+
+#define MFIO_42_FUNC_SEL_START   30
+#define MFIO_42_FUNC_SEL_END     30
+
+#define MFIO_43_FUNC_SEL_START   31
+#define MFIO_43_FUNC_SEL_END     31
+
+#define MFIO_45_FUNC_SEL_START   32
+#define MFIO_45_FUNC_SEL_END     32
+
+#define MFIO_54_FUNC_SEL_START   33
+#define MFIO_54_FUNC_SEL_END     34
+
+#define MFIO_55_FUNC_SEL_START   35
+#define MFIO_55_FUNC_SEL_END     36
+
+#define MFIO_56_FUNC_SEL_START   37
+#define MFIO_56_FUNC_SEL_END     38
+
+#define MFIO_57_FUNC_SEL_START   39
+#define MFIO_57_FUNC_SEL_END     40
+
+#define MFIO_58_FUNC_SEL_START   41
+#define MFIO_58_FUNC_SEL_END     41
+
+#define MFIO_63_FUNC_SEL_START   42
+#define MFIO_63_FUNC_SEL_END     43
+
+#define MFIO_64_FUNC_SEL_START   44
+#define MFIO_64_FUNC_SEL_END     45
+
+#define MFIO_65_FUNC_SEL_START   46
+#define MFIO_65_FUNC_SEL_END     47
+
+#define MFIO_66_FUNC_SEL_START   48
+#define MFIO_66_FUNC_SEL_END     49
+
+#define MFIO_67_FUNC_SEL_START   50
+#define MFIO_67_FUNC_SEL_END     51
+
+#define MFIO_68_FUNC_SEL_START   52
+#define MFIO_68_FUNC_SEL_END     53
+
+#define MFIO_69_FUNC_SEL_START   54
+#define MFIO_69_FUNC_SEL_END     55
+
+#define MFIO_70_FUNC_SEL_START   56
+#define MFIO_70_FUNC_SEL_END     57
+
+#define MFIO_73_FUNC_SEL_START   58
+#define MFIO_73_FUNC_SEL_END     59
+
+#define MFIO_74_FUNC_SEL_START   60
+#define MFIO_74_FUNC_SEL_END     61
+
+#define MFIO_75_FUNC_SEL_START   62
+#define MFIO_75_FUNC_SEL_END     63
+
+#define MFIO_76_FUNC_SEL_START   64
+#define MFIO_76_FUNC_SEL_END     65
+
+#define MFIO_77_FUNC_SEL_START   66
+#define MFIO_77_FUNC_SEL_END     67
+
+#define MFIO_78_FUNC_SEL_START   68
+#define MFIO_78_FUNC_SEL_END     69
+
+#define MFIO_79_FUNC_SEL_START   70
+#define MFIO_79_FUNC_SEL_END     71
+
+#define MFIO_80_FUNC_SEL_START   72
+#define MFIO_80_FUNC_SEL_END     73
+
+#define MFIO_81_FUNC_SEL_START   74
+#define MFIO_81_FUNC_SEL_END     75
+
+#define MFIO_82_FUNC_SEL_START   76
+#define MFIO_82_FUNC_SEL_END     77
+
+#define MFIO_83_FUNC_SEL_START   78
+#define MFIO_83_FUNC_SEL_END     79
+
+#define MFIO_84_FUNC_SEL_START   80
+#define MFIO_84_FUNC_SEL_END     81
+
+#define MFIO_85_FUNC_SEL_START   82
+#define MFIO_85_FUNC_SEL_END     83
+
+#define MFIO_86_FUNC_SEL_START   84
+#define MFIO_86_FUNC_SEL_END     85
+
+#define MFIO_87_FUNC_SEL_START   86
+#define MFIO_87_FUNC_SEL_END     87
+
+#define MFIO_88_FUNC_SEL_START   88
+#define MFIO_88_FUNC_SEL_END     89
+
+#define MFIO_89_FUNC_SEL_START   90
+#define MFIO_89_FUNC_SEL_END     91
+
+struct pistachio_mfio_reg {
+	unsigned int startbit;
+	unsigned int endbit;
+};
+
+void mfio_setup_mmc(void);
+void mfio_setup_ethernet(void);
